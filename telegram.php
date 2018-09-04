a33d7cd292b8c4458ee324bd104e9b691157b90c
Mellowcall <martincall@yandex.ru>
2018-09-04 12:32:29 +0300

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot688280998:AAG6hifgly4kH3sINqq-mv--CjgXL3lDRLA/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$token = "688280998:AAG6hifgly4kH3sINqq-mv--CjgXL3lDRLA";
+$chat_id = "288479675";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: Thanks.html');
+} else {
+  echo "Error";
+}
+?>
